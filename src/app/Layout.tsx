ea1adff7252ce91bd7154d1d859d06ee0a807674
Kalash Sharma <kalash.strt@gmail.com>
2026-01-10 21:43:02 +0530

    add: responsive layout design for page content

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -6,14 +6,18 @@ import Navbar from "../components/navbar";
 function Layout() {
   return (
     <div className="layout h-dvh">
-      <div className="layout__navbar flex items-center justify-center py-5">
+      <div className="layout__navbar flex items-center justify-center py-5 my-3">
         <Navbar />
       </div>
 
-      <div className="md:flex md:w-full">
-        <div className="layout__illustration md:w-4/12">Illustration</div>
+      <div className="w-full flex items-center justify-center">
+        <div className="flex flex-col md:flex-row w-full md:w-9/12">
+          <div className="layout__illustration md:w-5/12 bg-red-500">
+            Illustration
+          </div>
 
-        <div className="layout__content md:w-8/12">Content</div>
+          <div className="layout__content md:w-7/12 bg-yellow-500">Content</div>
+        </div>
       </div>
 
       <div className="layout__footer flex items-center justify-center py-5">
