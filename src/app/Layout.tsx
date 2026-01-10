1dd5721232bde3e6008344a4c24774a3633c6bdf
Kalash Sharma <kalash.strt@gmail.com>
2026-01-10 20:07:53 +0530

    add: responsive layout skeleton

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,26 @@
 // imports
 
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+
 function Layout() {
-  return <div className="layout">Layout</div>;
+  return (
+    <div className="layout">
+      <div className="layout__navbar flex items-center justify-center py-5">
+        <Navbar />
+      </div>
+
+      <div className="md:flex md:w-full">
+        <div className="layout__illustration md:w-4/12">Illustration</div>
+
+        <div className="layout__content md:w-8/12">Content</div>
+      </div>
+
+      <div className="layout__footer flex items-center justify-center py-5">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
