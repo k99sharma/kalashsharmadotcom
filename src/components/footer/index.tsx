9cf0d0b37d4240660b9e80e965511aa4974f8e8b
Kalash Sharma <kalash.strt@gmail.com>
2026-01-11 18:25:40 +0530

    add: footer text

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,18 @@
 function Footer() {
-  return <div className="footer">Footer</div>;
+  const linkedInUrl: string = "https://linkedin.com/in/kalashsharma99";
+  const year: number = new Date().getFullYear();
+
+  return (
+    <div className="footer my-10 text-xs">
+      Designed & Developed by{" "}
+      <span>
+        <a className="hover:underline" href={linkedInUrl} target="_blank">
+          Kalash Sharma
+        </a>
+      </span>
+      {` © ${year}.  All right reserved.`}
+    </div>
+  );
 }
 
 export default Footer;
