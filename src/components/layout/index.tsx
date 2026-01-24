44a2d03d1c57c90a5376ea922d2df9662a1b1138
Kalash Sharma <kalash.strt@gmail.com>
2026-01-24 18:18:45 +0530

    add: map component in layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,17 @@
 // imports
 import Avatar from "../avatar";
+import Map from "../map";
 
 const Layout = () => {
   return (
     <div className="layout min-h-screen flex justify-center">
       <div className="layout__content bg-amber-500 w-full md:w-3/5 lg:w-6/12 flex flex-col items-center">
-        <div className="layout__content__header">
-          <div className="layout__content__header__map"></div>
-          <div className="layout__content__header__avatar">
+        <div className="layout__content__header w-5/6">
+          <div className="layout__content__header__map w-full relative z-0">
+            <Map />
+          </div>
+
+          <div className="layout__content__header__avatar relative z-10 -mt-22 md:-mt-28">
             <Avatar />
           </div>
         </div>
---
+++ src/components/map/index.tsx
@@ -0,0 +1,30 @@
+// imports
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import type { LatLngExpression } from "leaflet";
+
+const position: LatLngExpression = [28.6139, 77.209];
+
+const Map = () => {
+  return (
+    <div className="map">
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "200px", width: "100%", borderRadius: "12px" }}
+        scrollWheelZoom={false}
+        aria-hidden="true"
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={position}>
+          <Popup>Based in India 🇮🇳</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
