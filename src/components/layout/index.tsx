384941c3156299fa0818fcc1d357d696481b4698
Kalash Sharma <kalash.strt@gmail.com>
2026-01-25 05:26:23 +0530

    fix: responsive view patches

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,7 +10,7 @@ import Socials from "../socials";
 
 const Layout = () => {
   return (
-    <div className="layout min-h-screen flex justify-center select-none p-5">
+    <div className="layout min-h-screen flex justify-center select-none p-2 md:p-5">
       <div className="layout__content w-full md:w-3/5 lg:w-6/12 flex flex-col items-center my-20">
         <div className="layout__content__header w-5/6">
           <div className="layout__content__header__map w-full relative z-0">
